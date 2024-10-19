d1ac75c4ce8825548f6edbe6a0b914c807f25f69
Deciel <shongo021@gmail.com>
2024-10-19 09:40:06 +0200

    clear input on focus

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, {useEffect, useState} from 'react' 
 import cong from "./index"
-import { getDatabase, ref, onValue, set } from "firebase/database";
+import { getDatabase, ref, onValue, set } from 'firebase/database';
 
 
 const App = () => {
   const [data, setData] = useState([]);
-  const database = getDatabase(cong);
-  const messageRef = ref(database, "messages");
   const [itemList, setItemList] = useState({});
   const [count, setCount] = useState(0);
   const placeholder = "Was brauchen wir noch?";
   const [item, setItem] = useState(placeholder);
-
-
+  const database = getDatabase(cong);
+  const messageRef = ref(database, "messages");
+  
   useEffect(() => {
     // Initialize the Firebase database with the provided configuration
     
@@ -39,17 +38,24 @@ const App = () => {
     // Fetch data when the component mounts
     fetchData();
 
-  });
+  }, []);
   
   const addItem = (event) => {
+
+
+
     event.preventDefault();
+/*     setItemList(...itemList, item); */
     itemList[count] = item;
     set(messageRef, itemList);
     setItem(placeholder);
   };
 
   const removeItem = (index) => {
-    delete itemList.index;
+
+
+
+    delete itemList[index];
     setCount(count - 1);
     set(messageRef, itemList);
   };
@@ -64,12 +70,12 @@ const App = () => {
           <li key={index} className='flex items-center justify-start w-80 h-11'>
             <input className='rounded-full peer' type="checkbox" id={item} ></input>
             <label className='ml-5 text-xl tracking-wider peer-checked:line-through w-full text-left' for={item}>{item}</label>
-            <button onClick={removeItem(index)} className='text-2xl hover:text-red-500 hover:text-3xl cursor-pointer active:text-2xl'>x</button>
+            <button onClick={() => removeItem(index)} className='text-2xl hover:text-red-500 hover:text-3xl cursor-pointer active:text-2xl'>x</button>
             </li>
         ))}
       </ul>
       <form className='mb-16' onSubmit={addItem}>
-        <input className='w-64 bg-white border-2 border-pink-600 rounded-full h-11 p-5' type='text' placeholder={item} onChange={(e) => {setItem(e.target.value); setCount(count+1)}}></input>
+        <input className='w-64 bg-white border-2 border-pink-600 rounded-full h-11 p-5 text-gray-600' type='text' placeholder={item} value={item} onFocus={(e) => {e.target.value = '';}} onChange={(e) => {setItem(e.target.value); setCount(count+1)}}></input>
         <button className='bg-white w-11 h-11 ml-6 text-black rounded-full border-pink-600 border-2' type='submit'>&gt;</button>
       </form>
     </div>
